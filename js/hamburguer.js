47c44c0872a0a0a070c756afd271a3abdc41ec85
Juan Valero <valerojuan13c@gmail.com>
2021-10-14 11:22:25 -0300

    Add Services

--- js/hamburguer.js
+++ js/hamburguer.js
@@ -1,6 +1,9 @@
 export function hamburguer(btn, menu) {
     const $btn = document.querySelector(btn);
     const $menu = document.querySelector(menu);
+    const noScroll = () => {
+      window.scrollTo(0,0);
+    };
   
     const close = `
           <span class="material-icons">
@@ -16,15 +19,18 @@ export function hamburguer(btn, menu) {
   
     $btn.addEventListener("click", (e) => {
       if ($btn.textContent.trim() === "menu") {
+        window.addEventListener("scroll", noScroll)
         $btn.innerHTML = close;
         $menu.classList.toggle('menu-active');
       } else {
+        window.removeEventListener("scroll", noScroll);
         $btn.innerHTML = open;
         $menu.classList.toggle('menu-active');
       }
     });
 
     $menu.addEventListener("click", (e) => {
+      window.removeEventListener("scroll", noScroll);
       $btn.innerHTML = open;
       $menu.classList.toggle('menu-active');
     })
